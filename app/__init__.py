e650b32d95048a2a325ca17ea7ae59a757a62343
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2024-12-29 14:05:14 +0300

    Implement form handling for the login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask
+'''This file initializes the flask application 
+    and organizes the application set'''
+
+from flask import Flask # type: ignore
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
---
+++ config.py
@@ -0,0 +1,5 @@
+# Class for secret key configuration
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
