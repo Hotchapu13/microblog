c8de959628d344070bbd9202c27d0ab626a175e7
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2024-12-30 20:10:23 +0300

    Replace haedcoded links with link generators

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect  # type: ignore
+from flask import render_template, flash, redirect, url_for  # type: ignore
 from app import app
 from app.forms import LoginForm
 
@@ -57,5 +57,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
