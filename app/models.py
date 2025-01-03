089588f178bec10b283d2bb7f813c94e76886b23
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2025-01-03 23:04:18 +0300

    Add login functionality

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from app import db
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
+from flask_login import UserMixin # type: ignore
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -25,4 +27,8 @@ class Post(db.Model):
     
     def __repr__(self):
         return '<post {}>'.format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
     
