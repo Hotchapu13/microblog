abb67533f95d5a5233dd418ad53764108d74d014
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2025-01-08 11:34:01 +0300

    Implement user login requirement

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from flask import render_template, flash, redirect, url_for  # type: ignore
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user # type: ignore
-from app.models import User # type: ignore
+from flask_login import current_user, login_user, login_required, logout_user  # type: ignore
+from app.models import User  # type: ignore
+from werkzeug.urls import url_parse # type: ignore
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -55,17 +58,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # handle the situation where an already logged in user navigates to the login url
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data )
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
     return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/logout')
 def logout():
