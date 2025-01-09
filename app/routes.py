710230bf37f44c744e04fbe393fffca027df18d2
Nuwe Bryant Nyero <nuwebryant@gmail.com>
2025-01-09 20:34:37 +0300

    Remove dummy user and add functionality to get user from database for authentication

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,15 @@
-from flask import render_template, flash, redirect, url_for  # type: ignore
+from flask import render_template, flash, redirect, url_for, request  # type: ignore
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, login_required, logout_user  # type: ignore
 from app.models import User  # type: ignore
-from werkzeug.urls import url_parse # type: ignore
+from urllib.parse import urlparse  # type: ignore
 
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -53,7 +52,7 @@ def index():
             'body': 'Assure me!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -69,9 +68,8 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('index')
     return render_template('login.html', title='Sign In', form=form)
 
