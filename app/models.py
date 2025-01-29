8bfaad9e623d165477e4d0117ee007667afe488d
Bryant <nuwebryant@gmail.com>
2025-01-29 16:54:38 +0300

    Add follower and following relationship

--- app/models.py
+++ app/models.py
@@ -5,13 +5,18 @@ from werkzeug.security import generate_password_hash, check_password_hash # type
 from flask_login import UserMixin # type: ignore
 from app import login
 
+
+followers = db.Table('followers', db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
 class User(UserMixin, db.Model):    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    followed = db.relationship('User', secondary=followers, primaryjoin=(followers.c.follower_id == id), secondaryjoin=(followers.c.followed_id == id), backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -25,7 +30,7 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
-
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
